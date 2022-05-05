12ea6d694232a8d9764da487de2520c3fcaec4d3
Oscar Davine <oscardavine@gmail.com>
2022-05-05 20:52:34 +1000

    alpha version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 
 client = discord.Client()
-
+country_name = 'Invalid'
 
 @client.event
 async def on_ready():
@@ -13,16 +13,24 @@ async def on_ready():
 @client.event
 async def on_message(message):
     """Function for text commands that require responses"""
-    global help
+    global country_name
+    
     if message.author == client.user:
         return
 
-    elif message.content.startswith('$country'):
+    if message.content.startswith('$country'):
         # uploads random meme from folder
         random_country=random.choice(os.listdir("countries"))
-        country_name = random_country[:]
+        country_name = random_country[:-4]
         await message.channel.send(file=discord.File(f'countries\{random_country}'))
+    
+    if message.content.startswith('$country'):
+        await message.channel.send(f'Try guess this country!')
 
+    elif message.content.lower().startswith(country_name.lower()):
+        author_id = message.id
+        country_name = 'Invalid'
+        await message.channel.send(f'You were correct {author_id}')
 
 with open('token') as token:
     TOKEN = token.readlines()[0]
